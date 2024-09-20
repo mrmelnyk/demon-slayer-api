424d31a56a46c396743da134b71054fbba07e466
MrMelnyk <mrmelnyk.dev@gmail.com>
2024-09-20 10:35:25 -0600

    Adding in more info to the api.

--- server.js
+++ server.js
@@ -10,41 +10,84 @@ const characters = {
         'fullName': 'Tanjiro Kamado',
         'race': 'Human',
         'gender': 'Male',
-        'age': '13 to 16',
+        'age': '16',
         'height': '165cm (5.5)',
         'weight': '61kg (134lb)',
-        'breathingTechnique': 'Water/Sun (Hirokami Kagara)'
+        'birthday': 'July 14',
+        'hairColor': 'Black to Red',
+        'eyeColor': 'Dark Red',
+        'combatStyle': 'Water Breathing/Sun Breathing (Hirokami Kagara)',
+        'affiliation': 'Demon Slayer Corps'
     },
 
     'zenistu': {
         'fullName': 'Zenitsu Agatsuma',
         'race': 'Human',
-        'breathingTechnique': 'Thunder'
+        'gender': 'Male',
+        'age': '16',
+        'height': '164cm (5.4)',
+        'weight': '58kg (128lbs)',
+        'birthday': 'September 3',
+        'hairColor': 'Blonde to Orange',
+        'eyeColor': 'Brown',
+        'combatStyle': 'Thunder Breathing',
+        'affiliation': 'Demon Slayer Corps'
     },
 
     'inosuke': {
         'fullName': 'Inosuke Hashibira',
         'race': 'Human',
-        'breathingTechnique': 'Beast'
+        'gender': 'Male',
+        'age': '15',
+        'height': '164cm (5.4)',
+        'weight': '63kg (139lbs)',
+        'birthday': 'April 22',
+        'hairColor': 'Black to Blue',
+        'eyeColor': 'Green',
+        'combatStyle': 'Beast Breathing',
+        'affiliation': 'Demon Slayer Corps'
     },
 
     'nezuko': {
         'fullName': 'Nezuko Kamado',
-        'race': 'Demon',
-        'breathingTechnique': 'N/A',
-        'bloodDemonArt': 'Burning Blood'
+        'race': 'Human/Demon',
+        'gender': 'Female',
+        'age': '14',
+        'height': '153cm (5.0)',
+        'weight': '45kg (99lbs)',
+        'birthday': 'December 28',
+        'hairColor': 'Black to Orange',
+        'eyeColor': 'Pink',
+        'combatStyle': 'Blood Demon Art: Combustible Blood',
+        'affiliation': 'Demon Slayer Corps'
     },
 
     'giyu': {
-        'fullName': 'Giyu Tomioka',
+        'fullName': 'Giyu Tomoika',
         'race': 'Human',
-        'breathingTechnique': 'Water'
+        'gender': 'Male',
+        'age': '21',
+        'height': '176cm (5.9)',
+        'weight': '69kg (152lbs)',
+        'birthday': 'February 8',
+        'hairColor': 'Black',
+        'eyeColor': 'Blue',
+        'combatStyle': 'Water Breathing',
+        'affiliation': 'Demon Slayer Corps'
     },
 
     'unknown': {
-        'fullName': 'Unknown',
-        'race': 'Unknown',
-        'breathingTechnique': 'Unknown'
+        'fullName': '',
+        'race': '',
+        'gender': '',
+        'age': '',
+        'height': '',
+        'weight': '',
+        'birthday': '',
+        'hairColor': '',
+        'eyeColor': '',
+        'combatStyle': '',
+        'affiliation': ''
     }
     
 }
