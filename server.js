70cae9ef57821dcf70e2729457448c1fb75d5ba5
MrMelnyk <mrmelnyk.dev@gmail.com>
2024-09-20 07:14:45 -0600

    Starting a Demon Slayer API

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+const characters = {
+    'tanjiro': {
+        'fullName': 'Tanjiro Kamado',
+        'race': 'Human',
+        'breathingTechnique': 'Water/Hirokami Kagara'
+    },
+
+    'zenistu': {
+        'fullName': 'Zenitsu Agatsuma',
+        'race': 'Human',
+        'breathingTechnique': 'Thunder'
+    },
+
+    'inosuke': {
+        'fullName': 'Inosuke Hashibira',
+        'race': 'Human',
+        'breathingTechnique': 'Beast'
+    },
+
+    'unknown': {
+        'fullName': 'Unknown',
+        'race': 'Unknown',
+        'breathingTechnique': 'Unknown'
+    }
+    
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (req, res) => {
+    const characterName = request.params.name.toLowerCase();
+    if(characters[characterName]){
+        res.json(characters[characterName]);
+    }else {
+        res.json(characters['unknown']);
+    };
+})
+
+app.listen(process.env.PORT || PORT, (req, res) => {
+    console.log(`The server is now running on port ${PORT}.`);
+})
