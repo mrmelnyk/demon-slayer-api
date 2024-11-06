e34fec2ee36e1e0d95fdf111f310909e8b7ab84d
MrMelnyk <mrmelnyk.dev@gmail.com>
2024-11-05 21:00:01 -0700

    Character variations.

--- server.js
+++ server.js
@@ -590,6 +590,22 @@ const characters = {
                 'power': 80
             },
 
+            'nezuko mt natagumo': {
+                'fullName': 'Nezuko Kamado',
+                'race': 'Human/Demon',
+                'gender': 'Female',
+                'age': '14',
+                'height': '153cm (5.0)',
+                'weight': '45kg (99lbs)',
+                'birthday': 'December 28',
+                'hairColor': 'Black to Orange',
+                'eyeColor': 'Pink',
+                'combatStyle': 'Blood Demon Art: Combustible Blood',
+                'affiliation': 'Demon Slayer Corps',
+                'arc': 'Unwavering Resolve: Mt. Natagumo',
+                'power': 90
+            },
+
             'muzan': {
                 'fullName': 'Muzan Kibutsuji',
                 'race': 'Demon',
